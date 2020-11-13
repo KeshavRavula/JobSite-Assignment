a6dc4881011e5ab78e063f278b5a00467befba5a
KeshavRavula <ravulakeshav9@gmail.com>
2020-11-13 22:48:15 +0530

    First Commit

---
+++ jobsearcher/src/test/java/com/indeeded/jobsearcher/JobsearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indeeded.jobsearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsearcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
