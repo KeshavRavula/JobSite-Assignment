d6689e3d6383cee9e528a8e8a839e13a52d6f6e1
saikrishnaboddu <39978856+saikrishnaboddu@users.noreply.github.com>
2020-11-16 11:35:54 +0530

    Added User service

--- jobsearcher/src/main/java/com/indeeded/jobsearcher/controller/UserController.java
+++ jobsearcher/src/main/java/com/indeeded/jobsearcher/controller/UserController.java
@@ -9,38 +9,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 import com.indeeded.jobsearcher.model.User;
+import com.indeeded.jobsearcher.service.UserService;
+
 //creating RestController
 @RestController
-@Service
 public class UserController
 {
     @Autowired
-    UserRepo userRepo;
+    UserService userService;
     //@RequestMapping("/user")
     @GetMapping("/user")
     private List<User> getAllUsers()
     {
-        List<User> users = new ArrayList<User>();
-        userRepo.findAll().forEach(user->users.add(user));
-        return users;
+        return userService.getAllUsers();
     }
     //creating a get mapping that retrieves the detail of a specific student
     @GetMapping("/user/{id}")
     private User getUser(@PathVariable("id") int id)
     {
-        return userRepo.findById(id).get();
+        return userService.getUser(id);
     }
     //creating a delete mapping that deletes a specific student
     @DeleteMapping("/user/{id}")
     private void deleteStudent(@PathVariable("id") int id)
     {
-        userRepo.deleteById(id);
+        userService.deleteUser(id);
     }
     //creating post mapping that post the student detail in the database
     @PostMapping("/user")
     private int saveStudent(@RequestBody User user)
     {
-        userRepo.save(user);
+        userService.saveUser(user);
         return Integer.parseInt(user.getId());
     }
 }
---
+++ jobsearcher/src/main/java/com/indeeded/jobsearcher/service/UserService.java
@@ -0,0 +1,35 @@
+package com.indeeded.jobsearcher.service;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.indeeded.jobsearcher.model.User;
+import com.indeeded.jobsearcher.Repo.UserRepo;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepo userRepo;
+    public List<User> getAllUsers()
+    {
+        List<User> users = new ArrayList<User>();
+        userRepo.findAll().forEach(user -> users.add(user));
+        return users;
+    }
+    public User getUser(int id)
+    {
+        return userRepo.findById(id).get();
+    }
+    public void saveUser(User user)
+    {
+        userRepo.save(user);
+    }
+    public void deleteUser(int id)
+    {
+        userRepo.deleteById(id);
+    }
+}
